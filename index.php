fa5eb5754645785deb3127df3f393cb470d39884
Vivek Kapoor <subs.vk@exain.com>
2024-03-24 17:37:37 +0000

    Added Index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- new.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Flash Briefing Feed</title>
+</head>
+<body>
+    <h1>Flash Briefing Feed</h1>
+
+<ul>
+<li> Append <code>/new</code> in the URL to create a new feed
+<li> In case you wish to view your feed, please use the link you had bookmarked. There's no way to search for avaiable feeds. It is by design as there is no authentication for any feed and anyone having the link can modify the feed.
+</ul>
+</body>
+</html>
